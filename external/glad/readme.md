0e017962895239dba587b5a072211a0f3ba711a4
Denis <36292357+AUXLander@users.noreply.github.com>
2022-04-09 17:08:02 +0300

    create simple open gl view template

---
+++ external/glad/readme.md
@@ -0,0 +1,6 @@
+source code avaible on: https://glad.dav1d.de/#language=c&specification=gl&api=gl%3D4.6&api=gles1%3Dnone&api=gles2%3Dnone&api=glsc2%3Dnone&profile=core&loader=on
+
+extract all files this directory:
+/inlude/glad/glad.h
+/inlude/KHR/khrplatform.h
+/src/glad.c
---
+++ simple-view/src/main.cpp
@@ -0,0 +1,55 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+    int exit_code = 0;
+
+    /* Initialize the library */
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if (window)
+    {
+        /* Make the window's context current */
+        glfwMakeContextCurrent(window);
+
+        if (gladLoadGL())
+        {
+            std::cout << "Render device: " << glGetString(GL_RENDERER) << '\n';
+            std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << '\n';
+
+            glClearColor(0, 1, 0, 1);
+
+            /* Loop until the user closes the window */
+            while (!glfwWindowShouldClose(window))
+            {
+                /* Render here */
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                /* Swap front and back buffers */
+                glfwSwapBuffers(window);
+
+                /* Poll for and process events */
+                glfwPollEvents();
+            }
+        }
+        else {
+            exit_code = -1;
+        }
+    }
+    else {
+        exit_code = -1;
+    }
+
+    glfwTerminate();
+
+    return exit_code;
+}
